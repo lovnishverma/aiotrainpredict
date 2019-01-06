78a8dddd58ccd93b0bfe638311bc0bc37c07f426
Glitch (flask-hello-world) <none>
2019-01-06 10:57:36 +0000

    🐈⛲️ Checkpoint ./server.py:2706322/125 ./views/app.html:2706322/32

--- server.py
+++ server.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from flask import Flask
+import os
+from flask import Flask, render_template
 
 app = Flask(__name__, static_folder='public', template_folder='views')
 
 @app.route('/')
 def home():
-    return render_template('app.html')
+    print("made by " + os.environ.get("MADE_BY"))
+    return render_template('app.html', name=os.environ.get("MADE_BY"))
 
 if __name__ == '__main__':
     app.run()

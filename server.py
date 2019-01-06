05c1ef0e2a151e371c4f51e402b8e7bdecf35566
Glitch (flask-hello-world) <none>
2019-01-06 14:41:38 +0000

    🎭☝️ Checkpoint ./server.py:2706322/534

--- server.py
+++ server.py
@@ -3,12 +3,31 @@
 
 import os
 from flask import Flask, render_template
+import sqlite3
 
 app = Flask(__name__, static_folder='public', template_folder='views')
 
-@app.route('/')
-def home():
-    return render_template('app.html', maker=os.environ.get("MADE_BY"))
+conn = sqlite3.connect('example.db')
+c = conn.cursor()
 
-if __name__ == '__main__':
-    app.run()
+# Create table
+c.execute('''CREATE TABLE IF NOT EXISTS stocks
+             (date text, trans text, symbol text, qty real, price real)''')
+
+# Insert a row of data
+c.execute("INSERT INTO stocks VALUES ('2006-01-05','BUY','RHAT',100,35.14)")
+
+# Save (commit) the changes
+conn.commit()
+
+# We can also close the connection if we are done with it.
+# Just be sure any changes have been committed or they will be lost.
+conn.close()
+
+
+#@app.route('/')
+#def home():
+#    return render_template('app.html', maker=os.environ.get("MADE_BY"))
+
+#if __name__ == '__main__':
+#    app.run()

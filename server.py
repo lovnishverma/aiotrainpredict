7cf4e2f775f4dc225f8e63c1988811cedea5321d
Glitch (coffee-pancreas) <none>
2019-01-06 20:02:42 +0000

    ⛈🚃 Checkpoint ./server.py:2706322/1373

--- server.py
+++ server.py
@@ -7,38 +7,27 @@ import sqlite3
 
 app = Flask(__name__, static_folder='public', template_folder='views')
 
-conn = sqlite3.connect('example.db')
-c = conn.cursor()
-
 def table_exists (cursor, name):
   cursor.execute('''SELECT name FROM sqlite_master 
                     WHERE type='table' AND name='{}';'''.format(name) )
   res = cursor.fetchall()
-  print("res: " + str(res))
-  print("result: " + str(len(res)))
   return len(res) > 0
-  
-# Save (commit) the changes
-conn.commit()
 
-# We can also close the connection if we are done with it.
-# Just be sure any changes have been committed or they will be lost.
-conn.close()
-
+def fill_table (cursor):
+  if not table_exists(c, "todos"):
+    c.execute('''CREATE TABLE IF NOT EXISTS todos 
+                 (id int, todo text)''')
+    c.execute('''INSERT INTO todos VALUES ("Boodschappen");''')
+    c.execute('''INSERT INTO todos VALUES ("Geld halen");''')
+    c.execute('''INSERT INTO todos VALUES ("Boek lezen");''')  
 
 @app.route('/')
 def home():
     print("home")
     conn = sqlite3.connect('example.db')
     c = conn.cursor()
-    if not table_exists(c, "todos"):
-      c.execute('''CREATE TABLE IF NOT EXISTS todos 
-                   (id int, todo text)''')
-      c.execute('''INSERT INTO todos VALUES ("Boodschappen");''')
-      c.execute('''INSERT INTO todos VALUES ("Geld halen");''')
-      c.execute('''INSERT INTO todos VALUES ("Boek lezen");''')
+    fill_table(c)
 
-    
     c.execute('''SELECT rowid, todo FROM todos''')
     res = c.fetchall()
     print("result: " + str(res))

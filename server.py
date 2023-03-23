b42a1bb831dc08fe8b815331e446b7007a1ba9fe
Glitch (spring-event) <none>
2023-03-23 07:15:21 +0000

    🎲🙇 Checkpoint ./server.py:5437171/455

--- server.py
+++ server.py
@@ -8,3 +8,13 @@ def defaultroot():
 
 if __name__ == '__main__':
   app.run()
+  
+'''
+CREATE TABLE students
+             (studentid integer  primary key AUTOINCREMENT,
+              name varchar(30) NOT NULL,
+              email varchar(40) NOT NULL,
+              passwd varchar(20) NOT NULL) ;
+ insert into students ( name, email, passwd) values (  'Ram' , 'ram@gmail.com','rr')            
+              
+'''

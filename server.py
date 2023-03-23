cb1e3523ab021b35fc145be92f74864e5a53bf13
Glitch (spring-event) <none>
2023-03-23 07:03:21 +0000

    🤑🌷 Checkpoint ./start.sh:5437171/140 ./server.py:5437171/1756 ./requirements.txt:5437171/27

--- server.py
+++ server.py
@@ -1,62 +1,10 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
+from flask import *  
 
-import os
-from flask import Flask, render_template
-import pymongo
-import json
-from bson import json_util
+app = Flask(__name__)
 
-app = Flask(__name__, static_folder='public', template_folder='views')
-
-def fill_collection (todos):
-    todos.insert_one({"id": 1, "todo": "Boodschappen"})
-    todos.insert_one({"id": 2, "todo": "Geld halen"})
-    todos.insert_one({"id": 3, "todo": "Boek lezen"})   
-
-@app.route('/createdb')
-def createdb(data):
-    todo = json_util.loads(data) # convert from JSON format
-  
-    mongo_url = os.environ.get("MONGO_URL")
-    client = pymongo.MongoClient(mongo_url) ## /demodb???
-    db = client['demodb']
-    
-    #db = client.demodb
-    
-    
-@app.route('/todo')
-def new_todo(data):
-    todo = json_util.loads(data) # convert from JSON format
-  
-    mongo_url = os.environ.get("MONGO_URL")
-    client = pymongo.MongoClient(mongo_url) ## /demodb???
-    db = client.demodb
-    todos = db.todos
-    
-    todos.insert_one(todo)
-    
-    
-    return "OK"
-    
-    
-@app.route('/todos')
-def get_todos():
-    mongo_url = os.environ.get("MONGO_URL")
-    client = pymongo.MongoClient(mongo_url) ## /demodb???
-    
-    todos = client.demodb.todos
-    if todos.count_documents({}) == 0:
-        fill_collection(todos)
-    todolist = list(todos.find())
-    print(json_util.dumps(todolist))
-    client.close()
-    return json_util.dumps(todolist)
-    
 @app.route('/')
-def home():
-    print("home")
-    return render_template('app.html', maker=os.environ.get("MADE_BY"))
+def defaultroot():
+  return 'Hello, World!'
 
 if __name__ == '__main__':
-    app.run()
+  app.run()

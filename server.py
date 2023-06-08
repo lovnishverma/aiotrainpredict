470ddaf7de9c3f7f2ec56a2017b01fc677ce9030
Glitch (spring-event) <none>
2023-06-08 14:15:37 +0000

    🔋🚗 Checkpoint ./rain.py:57629255/575 ./result.py:57629255/585 ./server.py:57629255/1946 ./templates/rain.html:57629255/579

--- server.py
+++ server.py
@@ -12,23 +12,6 @@ def index():
 def rain_page():
     return render_template("rain.html")
 
-@app.route('/rain', methods=["POST"])
-def predict_rain():
-    sw = float(request.form.get("sw"))
-    sh = float(request.form.get("sh"))
-    pw = float(request.form.get("pw"))
-    
-    url = "https://raw.githubusercontent.com/priyanka9-99/aiot/main/test.csv"
-    df = pd.read_csv(url)
-    X = df.iloc[:, 0:3].values  # Input features
-    Y = df.iloc[:, 3].values    # Output variable
-
-    model = LogisticRegression()
-    model.fit(X, Y)
-    
-    rainfall_prediction = model.predict([[sw, sh, pw]])[0]
-    
-    return render_template("rain.html", rainfall=rainfall_prediction)
 
 if __name__ == '__main__':
     app.run()

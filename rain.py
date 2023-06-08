5e729304d92b38afba2d3b9b939b5823a4f860f7
Glitch (spring-event) <none>
2023-06-08 16:14:22 +0000

    🌥☺️ Checkpoint ./rain.py:57629255/14188 ./server.py:57629255/10066 ./start.sh:57629255/24 ./templates/rain.html:57629255/88

--- rain.py
+++ rain.py
@@ -1,25 +1,30 @@
-from flask import *  
-import sqlite3
-import numpy   
-import pandas  as pd 
-from  sklearn.linear_model import LinearRegression 
+from flask import Flask, render_template, request
+import pandas as pd
 from sklearn.linear_model import LogisticRegression
 
 app = Flask(__name__)
-@app.route('/rain', methods=["POST"])
-def predict_rain():
+
+@app.route('/')
+def rainpage():
+    return render_template("rain.html")
+
+@app.route("/rain-predict", methods=["POST"])
+def rainpredict():
     sw = float(request.form.get("sw"))
     sh = float(request.form.get("sh"))
     pw = float(request.form.get("pw"))
     
     url = "https://raw.githubusercontent.com/priyanka9-99/aiot/main/test.csv"
-    df = pd.read_csv(url)
-    X = df.iloc[:, 0:3].values  # Input features
-    Y = df.iloc[:, 3].values    # Output variable
+    dfspf = pd.read_csv(url)
+    X = dfspf.iloc[:, 0:3]  # Select all rows and first three columns as input X
+    Y = dfspf.iloc[:, 3]    # Select all rows and the fourth column as output Y
+    
+    model2 = LogisticRegression()
+    model2.fit(X, Y)
+    
+    arr = model2.predict([[sw, sh, pw]])
+    
+    return render_template("rain.html", data=str(arr[0]) + " mm")
 
-    model = LogisticRegression()
-    model.fit(X, Y)
-    
-    rainfall_prediction = model.predict([[sw, sh, pw]])[0]
-    
-    return render_template("rain.html", rainfall=rainfall_prediction)
+if __name__ == '__main__':
+    app.run()
--- server.py
+++ server.py
@@ -1,33 +1,18 @@
-from flask import *  
-import sqlite3
-import numpy   
-import pandas  as pd 
-from  sklearn.linear_model import LinearRegression 
-from sklearn.linear_model import LogisticRegression
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-# @app.route('/')
-# def homepage():
-#     """Displays the homepage."""
-#     return render_template('index.html')
-
 @app.route('/')
-def rainpage():
-  return render_template("rain.html")
+def index():
+    return render_template("index.html")
 
-@app.route("/rain", methods=["POST"])
-def rainpredict():
-  sw = eval ( request.form.get ( "sw") )
-  sh = eval ( request.form.get ( "sh") )
-  pw = eval ( request.form.get ( "pw") )
-  url   = "https://raw.githubusercontent.com/priyanka9-99/aiot/main/test.csv"
-  dfspf = pd.read_csv(url)
-  df1   = dfspf.values
-  X = df1[:,0:3] # all rows and first two columns  becomes my input ie. X
-  Y = df1[:,3]   # all rows and only third column becomes my output ie Y 
-  model2 = LogisticRegression ()
-  model2.fit( X , Y )
-  arr   = model2.predict([[sw, sh, pw]] )
-  return render_template("rain.html" , data = str( arr[0] ) + " mm")
-  # return "Rain Prediction   :  " + str(arr[0] ) + " mm"
+@app.route('/rain')
+def about():
+    return render_template("rain.html")
+
+@app.route('/live_sensor_data')
+def contact():
+    return render_template("template.html")
+
+if __name__ == '__main__':
+    app.run()

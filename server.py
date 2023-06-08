87dea33d1b88a2d415955db140cf7aa294366ad8
Glitch (spring-event) <none>
2023-06-08 13:52:35 +0000

    🏣🌴 Checkpoint ./package.json:57629255/20 ./requirements.txt:57629255/25 ./server.py:57629255/5188 ./templates/index.html:57629255/81

--- server.py
+++ server.py
@@ -1,119 +1,34 @@
-from flask import *  
-import sqlite3
-import numpy   
-import pandas  as pd 
-from  sklearn.linear_model import LinearRegression 
+from flask import *
+import pandas as pd
 from sklearn.linear_model import LogisticRegression
 
 app = Flask(__name__)
 
-#   install following in glitch terminal 
-# pip install scikit-learn
-# pip install pandas
 @app.route('/')
-def rainpage():
-  return render_template("rain.html")
+def index():
+    return render_template("index.html")
 
-@app.route("/rain", methods=["POST"])
-def rainpredict():
-  sw = eval ( request.form.get ( "sw") )
-  sh = eval ( request.form.get ( "sh") )
-  pw = eval ( request.form.get ( "pw") )
-  url   = "https://raw.githubusercontent.com/priyanka9-99/aiot/main/test.csv"
-  dfspf = pd.read_csv(url)
-  df1   = dfspf.values
-  X = df1[:,0:3] # all rows and first two columns  becomes my input ie. X
-  Y = df1[:,3]   # all rows and only third column becomes my output ie Y 
-  model2 = LogisticRegression ()
-  model2.fit( X , Y )
-  arr   = model2.predict([[sw, sh, pw]] )
-  return render_template("rain.html" , data = str( arr[0] ) + " mm")
-  # return "Rain Prediction   :  " + str(arr[0] ) + " mm"
+@app.route('/rain')
+def rain_page():
+    return render_template("rain.html")
 
+@app.route('/predict_rain', methods=["POST"])
+def predict_rain():
+    sw = float(request.form.get("sw"))
+    sh = float(request.form.get("sh"))
+    pw = float(request.form.get("pw"))
+    
+    url = "https://raw.githubusercontent.com/priyanka9-99/aiot/main/test.csv"
+    df = pd.read_csv(url)
+    X = df.iloc[:, 0:3].values  # Input features
+    Y = df.iloc[:, 3].values    # Output variable
 
-@app.route('/irisp' , methods=["POST"])
-def irispredict():
-  sw = eval ( request.form.get ( "sw") )
-  sh = eval ( request.form.get ( "sh") )
-  pw = eval ( request.form.get ( "pw") )
-  ph = eval ( request.form.get ( "ph") )
-  ######### machine learning
-  url ="https://raw.githubusercontent.com/sarwansingh/Python/master/ClassExamples/data/iris.csv"
-  #nameslist = ['SepalLength', 'SepalWidth', 'PetalLength', 'PetalWidth', 'Species']
-  dfIris =  pd.read_csv(url, header=None ) # , names=nameslist)
-  df2 = dfIris.values
-  
-  #divide data into feature matrix and target matrix
-  X = df2[:,:4] # all the rows and first four columns 0-3
-  Y = df2[:,4]   # all the rows and only fourth column (last column )
-  
-  from sklearn.linear_model import LogisticRegression
-  model1= LogisticRegression()
-  model1.fit(X,Y)
-  arr  = model1.predict([[ sw, sh, pw, ph]])
-  ###############
-  return render_template("iris.html" , data = str( arr[0] ) )
-
-
-@app.route("/p", methods=["POST"])
-def predictresult():
-  hrsself = int ( request.form.get ( "hrsself") )
-  hrstut  = int ( request.form.get ( "hrstut") ) 
-  # predict and save the output in result variable
-  url   = "https://raw.githubusercontent.com/sarwansingh/Python/master/ClassExamples/data/student-pass-fail-data.csv"
-  dfspf = pd.read_csv(url)
-  df1   = dfspf.values
-  X = df1[:,0:2] # all rows and first two columns  becomes my input ie. X
-  Y = df1[:,2]   # all rows and only third column becomes my output ie Y 
-  model = LinearRegression ()
-  model.fit( X , Y )
-  arr   = model.predict([[hrsself,hrstut]] )
-  #################
-  result = arr[0] *100
-  return render_template("result.html", data = str(result) + "%")
-
-@app.route("/ml")
-def machinelearning():
-  url   = "https://raw.githubusercontent.com/sarwansingh/Python/master/ClassExamples/data/student-pass-fail-data.csv"
-  dfspf = pd.read_csv(url)
-  df1   = dfspf.values
-  X = df1[:,0:2] # all rows and first two columns  becomes my input ie. X
-  Y = df1[:,2]   # all rows and only third column becomes my output ie Y 
-  model = LinearRegression ()
-  model.fit( X , Y )
-  arr   = model.predict([[5,25]] )
-  return "Passing Prediction   :  " + str(arr[0] * 100) + "%"
-
-
-
-@app.route('/result')
-def resultpage():
-  return render_template("result.html")
-
-# @app.route('/')
-# def indexpage():
-#   return render_template("index.html")
-
-def defaultroot():
-  #get the data from database - db1 and table - students
-  con  = sqlite3.connect("db1")  # connect sms database
-  con.row_factory = sqlite3.Row  # create object of Row
-  cur = con.cursor()             
-  cur.execute( "select * from students")  # execute a SQL query to get the data
-  rows = cur.fetchall()          # all the data pulled from database is stored in rows object 
-  con.close ()
-  
-  return render_template("index.html", data=rows)
+    model = LogisticRegression()
+    model.fit(X, Y)
+    
+    rainfall_prediction = model.predict([[sw, sh, pw]])[0]
+    
+    return render_template("rain.html", rainfall=rainfall_prediction)
 
 if __name__ == '__main__':
-  app.run()
-  
-'''
-CREATE TABLE students
-             (studentid integer  primary key AUTOINCREMENT,
-              name varchar(30) NOT NULL,
-              email varchar(40) NOT NULL,
-              passwd varchar(20) NOT NULL) ;
-insert into students ( name, email, passwd) values ('Ram' , 'ram@gmail.com','rr') ;           
-insert into students ( name, email, passwd) values ('Sita', 'sita@gmail.com','ss');             
-'''
+    app.run()

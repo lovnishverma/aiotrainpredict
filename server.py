78bf4f01e1016b43b848b054e4b42cc564fed17b
Glitch (spring-event) <none>
2023-06-08 14:27:35 +0000

    🖐🔦 Checkpoint ./templates/rain.html:57629255/579 ./server.py:57629255/5208 ./result.py:57629255/615

--- server.py
+++ server.py
@@ -2,6 +2,8 @@ from flask import *
 import pandas as pd
 from sklearn.linear_model import LogisticRegression
 
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -12,6 +14,9 @@ def index():
 def rain_page():
     return render_template("rain.html")
 
+@app.route('/result')
+def live_sensor_data():
+    return render_template("result.html")
 
 if __name__ == '__main__':
     app.run()
